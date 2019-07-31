6a2ab9d74e6ecc52efcd8a34b92ca47b00e22971
Shun <shun8679@gmail.com>
2019-07-31 18:56:05 +0900

    Revert "殺すコミット"
    This reverts commit 637e64febfdcd7d00b47ed03be0347335ffeb871.

---
+++ Assets/Script/Animation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animation : MonoBehaviour
+{
+    public GameObject PlayerCurrentTex;
+    public Sprite[] PlayerTexNumber;
+
+    float time;
+    int i;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = 0.0f;
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+
+        if (time > 10.0f)
+        {
+            if (i == 0)
+                i = 1;
+            else
+                i = 0;
+
+            this.GetComponent<Image>().sprite = PlayerTexNumber[i];
+            time = 0.0f;
+        }
+
+    }
+
+}
