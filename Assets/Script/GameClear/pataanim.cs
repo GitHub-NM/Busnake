6708416437ce4447f3737e5a3d91dbe1c52d11ed
Shun <shun8679@gmail.com>
2019-08-09 22:24:32 +0900

    謎コミット

---
+++ Assets/Script/GameClear/pataanim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pataanim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localEulerAngles.x>=-180)
+        transform.Rotate(new Vector3(-5, 0, 0));
+        else if(transform.localEulerAngles.x >= 0)
+        {
+            transform.Rotate(new Vector3(0, 0, 0));
+        }
+
+        Debug.Log(transform.localEulerAngles.x);
+    }
+}
