05956531545360bfb3b4827efb889b0ec3c9faa5
kanaishida <kana.psvk.524@gmail.com>
2019-08-11 05:01:31 +0900

    タイトルSceneとステージセレクトSceneのシーン遷移のみ実装を追加しました

---
+++ Assets/Script/Select/Select.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Select : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouseclick,EnterKey,SpacekeyでStageSelect→Gamemainにシーン遷移
+        //PCの入力モードが半角になっていないとSpaceKeyが反応しない(かなしい)
+        if (Input.GetMouseButtonDown(0) || Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Gamemain");
+        }
+    }
+}
---
+++ Assets/Script/Title/Title.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouseclick,EnterKey,SpacekeyでTitle→Selectにシーン遷移
+        //PCの入力モードが半角になっていないとSpaceKeyが反応しない(かなしい)
+        if (Input.GetMouseButtonDown(0)|| Input.GetKey(KeyCode.Return)|| Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("StageSelect");
+        }
+        
+        //初回起動のみ続きからを表示しない
+        //続きからを選んだ場合、セーブデータにアクセスしてデータをロードする処理を作る
+       
+    }
+}
